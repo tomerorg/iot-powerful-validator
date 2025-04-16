93a3b5ea484bd272bff4b12a68f35af1a4083b8e
tomerbr3456 <59730830+tomerbr3456@users.noreply.github.com>
2025-04-16 13:55:20 +0300

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,18 @@
+
+from setuptools import setup, find_packages
+
+setup(
+    name="iot-powerful-validator",
+    version="0.1.0",
+    packages=find_packages(),
+    install_requires=[
+        "requests>=2.25.1",
+        "pandas>=1.2.0",
+        "numpy>=1.20.0",
+    ],
+    author="",
+    author_email="",
+    description="Iot service implementing Django and Pandas architecture",
+    keywords="iot-powerful-validator, python",
+    url="",
+)
